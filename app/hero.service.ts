5c41d91999f8657337a479a5056d8bf012cce7ff
Huy <tongnguyen.hahuy@gmail.com>
2016-07-06 05:06:15 +0700

    http

--- app/hero.service.ts
+++ app/hero.service.ts
@@ -4,7 +4,7 @@
 import {Injectable} from '@angular/core';
 import {HEROES} from './mock-heroes'
 import {Hero} from "./hero";
-import {Http} from "@angular/http";
+import {Http, Headers} from "@angular/http";
 import 'rxjs/add/operator/toPromise';
 
 
@@ -15,6 +15,11 @@ export class HeroService {
 
   constructor(private http: Http) {
 
+  }
+
+  private handleError(error: any) {
+    console.error('An error occurred', error);
+    return Promise.reject(error.message || error);
   }
 
   // getHeroes() {
@@ -38,5 +43,51 @@ export class HeroService {
   getHero(id: number) {
     return this.getHeroes()
       .then(heroes => heroes.filter(hero => hero.id === id)[0]);
+  }
+
+  // Add new Hero
+  private post(hero: Hero): Promise<Hero> {
+    let headers = new Headers({
+      'Content-Type': 'application/json'
+    });
+    return this.http
+      .post(this.heroesUrl, JSON.stringify(hero), {headers: headers})
+      .toPromise()
+      .then(res => res.json().data)
+      .catch(this.handleError);
+  }
+
+  // update existing Hero
+  private put(hero: Hero) {
+    let headers = new Headers();
+    headers.append('Content-Type', 'application/json');
+
+    let url = `${this.heroesUrl}/${hero.id}`
+
+    return this.http
+      .put(url, JSON.stringify(hero), {headers: headers})
+      .toPromise()
+      .then(() => hero)
+      .catch(this.handleError);
+  }
+
+  // Delete hero
+  delete(hero: Hero) {
+    let headers = new Headers();
+    headers.append('Content-Type', 'application/json');
+
+    let url = `${this.heroesUrl}/${hero.id}`;
+
+    return this.http
+      .delete(url, headers)
+      .toPromise()
+      .catch(this.handleError);
+  }
+
+  save(hero: Hero): Promise<Hero> {
+    if (hero.id) {
+      return this.put(hero);
+    }
+    return this.post(hero);
   }
 }
--- app/heroes.component.ts
+++ app/heroes.component.ts
@@ -2,24 +2,26 @@ import {Component, OnInit} from '@angular/core';
 import {Hero} from './hero';
 import {HeroService} from './hero.service';
 import {Router} from '@angular/router';
+import {HeroDetailComponent} from './hero-detail.component'
 
 @Component({
   selector: 'my-heroes',
   templateUrl: 'app/heroes.component.html',
   styleUrls: ['app/heroes.component.css'],
-  directives: [],
+  directives: [HeroDetailComponent],
   providers: []
 })
 export class HeroesComponent implements OnInit {
   title = 'Tour of Heroes';
   heroes:Hero[];
   selectedHero:Hero;
+  error: any;
+  addingHero: boolean;
 
   constructor(
     private heroService:HeroService,
     private router: Router
-  ) {
-  }
+  ) {}
 
   onSelect(hero:Hero) {
     this.selectedHero = hero;
@@ -35,6 +37,27 @@ export class HeroesComponent implements OnInit {
 
   gotoDetail() {
     this.router.navigate(['/detail', this.selectedHero.id]);
+  }
+
+  addHero() {
+    this.addingHero = true;
+    this.selectedHero = null;
+  }
+
+  close(savedHero: Hero) {
+    this.addingHero = false;
+    if (savedHero) { this.getHeroes(); }
+  }
+
+  deleteHero(hero: Hero, event: any) {
+    event.stopPropagation();
+    this.heroService
+      .delete(hero)
+      .then(res => {
+        this.heroes = this.heroes.filter(h => h !== hero);
+        if (this.selectedHero === hero) { this.selectedHero = null; }
+      })
+      .catch(error => this.error = error);
   }
 
 }
