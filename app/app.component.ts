6a17e9bf202742efd7e25f52571f46ec41501404
123456 <thuyuyly93@gmail.com>
2016-07-05 18:05:31 +0700

    http

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,11 +1,11 @@
-import { Component } from '@angular/core';
-import { ROUTER_DIRECTIVES } from '@angular/router';
+import {Component} from '@angular/core';
+import {ROUTER_DIRECTIVES} from '@angular/router';
 
-import { HeroService } from './hero.service';
+import {HeroService} from './hero.service';
 
 @Component({
-    selector: 'my-app',
-    template: `
+  selector: 'my-app',
+  template: `
         <h1>{{title}}</h1>
         <nav>
             <a [routerLink]="['/dashboard']" routerLinkActive="active">Dashboard</a>
@@ -13,10 +13,11 @@ import { HeroService } from './hero.service';
         </nav>    
         <router-outlet></router-outlet> 
     `,
-    directives: [ROUTER_DIRECTIVES],
-    providers: [HeroService]
+  styleUrls: ['app/app.component.css'],
+  directives: [ROUTER_DIRECTIVES],
+  providers: [HeroService]
 })
 export class AppComponent {
-    
-    title = 'Tour of Heroes';
+
+  title = 'Tour of Heroes';
 }
--- app/hero.service.ts
+++ app/hero.service.ts
@@ -4,17 +4,39 @@
 import {Injectable} from '@angular/core';
 import {HEROES} from './mock-heroes'
 import {Hero} from "./hero";
+import {Http} from "@angular/http";
+import 'rxjs/add/operator/toPromise';
+
 
 @Injectable()
 export class HeroService {
 
-  getHeroes() {
-    return Promise.resolve(HEROES);
+  private heroesUrl = 'app/heroes';  // URL to web api
+
+  constructor(private http: Http) {
+
+  }
+
+  // getHeroes() {
+  //   return Promise.resolve(HEROES);
+  // }
+
+  getHeroes(): Promise<Hero[]> {
+    return this.http.get(this.heroesUrl)
+      .toPromise()
+      .then(response => response.json().data)
+      .catch(this.handleError);
+
   }
 
   getHeroesSlowly() {
     return new Promise<Hero[]>(resolve =>
       setTimeout(() => resolve(HEROES), 5000) // 2 seconds
     );
+  }
+
+  getHero(id: number) {
+    return this.getHeroes()
+      .then(heroes => heroes.filter(hero => hero.id === id)[0]);
   }
 }
